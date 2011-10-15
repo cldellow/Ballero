eb9a268b47c5cd569bd7ade6c0e95a28d1e63f0f
Colin Dellow <cldellow@gmail.com>
2011-10-15 18:44:06 -0400

    Hello, world.

---
+++ src/test/scala/Specs.scala
@@ -0,0 +1,10 @@
+import cldellow.ballero
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.Spec
+
+class Specs extends Spec with ShouldMatchers {
+  describe("a spec") {
+    it("should do something") {
+    }
+  }
+}
